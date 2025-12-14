c13dfb5f55dd40599668fc9335c11c4aa6a0a017
Nigel John Angel <nigeljohnangel07@gmail.com>
2025-12-14 15:26:14 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -5,9 +5,9 @@
 
 int main() {
     int distance = 0;
-    int origin_location_ID = 102;
-    int origin_index;
-    int destination_location_ID = 113;
+    int origin_location_ID = 113;
+    int origin_index,i,destination_index;
+    int destination_location_ID = 100;
     char routefile[] = "locations.csv";
     int record_count = 0;
 
@@ -15,9 +15,31 @@ int main() {
     // Display the list
     displayList(Route, record_count);
 
-    printf("%d",record_count);
+    printf("%d\n",record_count);
 
-    printf("%d",origin_index);
+    int min_distance = record_count; // Start with the maximum possible stops
+
+    for (int i = 0; i < record_count; i++) {
+        if (Route[i].location_ID == origin_location_ID) {
+
+            for (int j = 0; j < record_count; j++) {
+                if (Route[j].location_ID == destination_location_ID) {
+
+                    // One-way Circular Math:
+                    // This formula always calculates the distance MOVING FORWARD
+                    int current_dist = (j - i + record_count) % record_count;
+
+                    // We still want the "shortest" because there are multiple
+                    // instances of the same ID in your CSV.
+                    if (current_dist < min_distance) {
+                        min_distance = current_dist;
+                    }
+                }
+            }
+        }
+    }
+
+    printf("The shortest distance in this one-way loop is %d km.\n", min_distance);
 
     free(Route);
     Route = NULL;

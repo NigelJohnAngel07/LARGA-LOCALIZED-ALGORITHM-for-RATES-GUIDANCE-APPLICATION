881cb856503c8296e0aedf723ceea604714d9954
Tyshaun Siga <149290608+bruh2912@users.noreply.github.com>
2025-12-15 23:19:27 +0800

    Add files via upload

---
+++ ERROR HANDLING main.c
@@ -0,0 +1,112 @@
+#include "route.h" // Include our custom header file
+
+
+// --- Main Program Logic ---
+
+int main() {
+    int distance = 0;
+    int origin_location_ID;
+    int origin_index,i,destination_index,ch,route_index;
+    int destination_location_ID;
+    char file[50];
+    int record_count = 0;
+    int totalRoutes = 0;
+    int choice;
+    float fare;
+    int valid = 0;//for error handling
+    int maxOption = 2; //error handling
+
+
+    printf("LARGA: LOCALIZED ALGORITHM for RATES GUIDANCE APPLICATION\n");
+    printf("Select Route");
+
+    Route *RouteSelection = loadRouteOptions("route_selection.csv", &totalRoutes);
+
+    if (RouteSelection == NULL) return 1;
+
+    while (!valid) {
+        printf("\nChoose a route index (0-%d): ", maxOption);
+        if (scanf("%d", &choice) == 1) {
+            if (choice < 0) {
+                printf("Invalid input. The index cannot be negative.\n");
+            }
+            else if (choice > maxOption) {
+                printf("Invalid input. Please choose an index between 0 and %d.\n", maxOption);
+            }
+            else {
+                valid = 1;
+            }
+        } else {
+            printf("Invalid input. Please enter a number.\n");
+        }
+        while ((ch = getchar()) != '\n' && ch != EOF);
+    }
+
+    strcpy(file, RouteSelection[choice].location_file);
+    Data *Route = createArray(file, &record_count);
+
+    displayList(Route, record_count);
+
+
+    //Origin Index Input
+    while (1) {
+        printf("Origin (0-21): ");
+
+        if (scanf("%d", &origin_index) == 1) {
+            while ((ch = getchar()) != '\n' && ch != EOF);
+            if (origin_index >= 0 && origin_index <= 21) {
+                break;
+            } else {
+                printf("Invalid input. Please choose an index between 0 and 21.\n");
+            }
+        } else {
+            printf("Invalid input. Please enter a number.\n");
+            while ((ch = getchar()) != '\n' && ch != EOF);
+        }
+    }
+
+    //Destination Index Input
+    while (1) { 
+        printf("Destination (0-21): ");
+
+       
+        if (scanf("%d", &destination_index) == 1) {
+
+            while ((ch = getchar()) != '\n' && ch != EOF);
+
+            if (destination_index >= 0 && destination_index <= 21) {
+                break; 
+            } else {
+                printf("Invalid input. Please choose an index between 0 and 21.\n");
+            }
+
+        } else {
+        
+            printf("Invalid input. Please enter a number.\n");
+
+
+            while ((ch = getchar()) != '\n' && ch != EOF);
+        }
+    }
+
+    origin_location_ID = Route[origin_index].location_ID;
+    destination_location_ID = Route[destination_index].location_ID;
+
+    free(Route);
+
+    strcpy(file, RouteSelection[choice].route_file);
+
+    Route = createArray(file,&record_count);
+
+    distance = calculateMinDistance(Route, record_count, origin_location_ID, destination_location_ID);
+    fare = calculateFare(distance);
+
+    displayReceipt(Route, origin_index, destination_index,distance, fare);
+
+    free(Route);
+    free(RouteSelection);
+    Route = NULL;
+    RouteSelection = NULL;
+
+    return 0;
+}
